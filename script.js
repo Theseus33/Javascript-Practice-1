1d29d1b14902c1a91aa093c04732ebd7426d8ddf
Rana Bhattacharyya <ranab33@gmail.com>
2019-05-14 22:41:09 -0400

    boolean logic

--- script.js
+++ script.js
@@ -162,6 +162,11 @@ var age = 16;
 
 if (age < 13) {
   console.log(firstName + 'is a boy.');
-} else if (){ //between 13 and 20;    
-
+} else if (age >= 13 && age > 20){
+    console.log(firstName + ' is a teenager.');    
+  } else if (age >= 20 && age < 30){   
+    console.log(firstName + ' is a young man.')
+} else {
+  console.log(firstName + ' is a man');
 }
+
