1685681959ce9832c3377176a712d3f2e660cc7f
Rana Bhattacharyya <ranab33@gmail.com>
2019-05-17 18:58:48 -0400

    coding challenge

--- script.js
+++ script.js
@@ -607,3 +607,50 @@ for (var i = john.length - 1; i >= 0; i--) {
   console.log(john[i]);
 }
 */
+
+/***********
+ * Coding Challenge
+ */
+
+/* Remember the tip calculator challenge? Let's create a more advanced version using everything we learned!
+
+ This time, John and his family went to 5 different restaurants. The bills were $124, $48, $268, $180, and $42.
+ John likes to tip 20% of the bill when the bill is less than $50., 15% if the bill is between $50 and $200, and 10%
+ if the bill is more than $200.
+
+ Implement a tip calculator using objects and loops: 
+ 1. Create and object with an array for the bill values.
+ 2.Add a method to calculate the tip
+ 3. Rhis method should include a lopp to iterateover all the paid bills and do the tip calculations
+ 4. s an output, create 1) a new array contaiing all the tips 2) an array contaiing final paid amounts (bill + tip.
+  HINT: Start with two empty arrays as poperties and fill them up in the loop.
+*/
+
+var john = {
+  fullName: "John Smith",
+  bills: [124, 48, 268, 180, 42],
+  calcTips: function() {
+    this.tips = [];
+    this.finalValues = [];
+
+    for (var i = 0; i < this.bills.length; i++) {
+      var bill = this.bills[i];
+      //Determine percentage based on tipping rules
+      var percentage;
+      if (bill < 50) {
+        percentage = 0.2;
+      } else if (bill >= 50 && bill < 200) {
+        percentage = 0.15;
+      } else {
+        percentage = 0.1;
+      }
+
+      //Add results to the corresponding arrays
+      this.tips[i] = bill * percentage;
+      this.finalValues[i] = bill + bill * percentage;
+    }
+  }
+};
+
+john.calcTips();
+console.log(john);
